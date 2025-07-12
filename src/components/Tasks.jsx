f124c0edf848c4a3faa2ed4972f8ab918e0b2903
Omar-Abd-ElAziz-Sheeth <your.email@example.com>
2025-07-12 15:27:28 +0300

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,16 +3,17 @@ import Task from "./Task";
 const Tasks = ({ tasks, removeHandler, toggle }) => {
   return (
     <ul>
-      {tasks.map((task, index) => (
-        <Task
-          key={index}
-          id={task.id}
-          text={task.text}
-          isComplete={task.isComplete}
-          removeHandler={removeHandler}
-          toggle={toggle}
-        />
-      ))}
+      {tasks &&
+        tasks.map((task, index) => (
+          <Task
+            key={index}
+            id={task.id}
+            text={task.text}
+            isComplete={task.isComplete}
+            removeHandler={removeHandler}
+            toggle={toggle}
+          />
+        ))}
 
       {tasks.length <= 0 && (
         <p className="text-red-600 text-center font-bold mt-12">
